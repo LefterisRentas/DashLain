8be0de4d301e9cfe7b8f08b1bda51f80bd1efb3e
Eleftherios Rentas <lefteris.rentasac@gmail.com>
2025-05-29 19:36:41 +0300

    rm

--- DashLain/Services/ProfileService.cs
+++ DashLain/Services/ProfileService.cs
@@ -74,10 +74,4 @@ public sealed class ProfileService {
         });
         return UIResult.Succeed(true);
     }
-
-    public async Task<UIResult<List<Profile>>> GetAllProfiles() {
-        return UIResult.Succeed((await _context.Profiles.Select(x => new Profile {
-            Name = x.Name,
-        }).ToListAsync()));
-    }
 }
